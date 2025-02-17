6da796be738bef83b7440d4d577b637f0a7f905b
zhujunzhe20050106 <1922459359@qq.com>
2025-02-17 09:50:52 +0800

    Delete 新建文件夹 directory

--- 新建文件夹/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('wechat-button').addEventListener('click', function() {
-    const qrcode = document.getElementById('wechat-qrcode');
-    if (qrcode.classList.contains('hidden')) {
-        qrcode.classList.remove('hidden');
-    } else {
-        qrcode.classList.add('hidden');
-    }
-});
